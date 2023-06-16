51a06d46e282896e0789d5cdb4e4c5393cff4633
Tizios9999 <davide.santonocito@outlook.com>
2023-06-16 16:05:12 +0200

    file upload frontend first ver ok

--- src/app/newticket/page.js
+++ src/app/newticket/page.js
@@ -8,7 +8,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
+import Input from '@mui/material/Input';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import CustomSelect from "../components/CustomSelect"
 
@@ -28,6 +31,10 @@ const NewTicket = () => {
     
     const [formState, setFormState] = useState(initialState);
     
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+
+    const [uploadActive, setUploadActive] = useState(false);
+
     function onChangeHandler(event) {
         let name = event.target.name;
         let value = event.target.value;
@@ -48,10 +55,43 @@ const NewTicket = () => {
         })
     }
 
+    function handleFileChange(event) {
+
+        let filesList = uploadedFiles;
+        
+        if (event.target.files && event.target.files[0]) {
+
+            // In order to upload the file, a check is made to know if another file with the same name exists
+
+            const newFilename = event.target.files[0].name;
+            const index = filesList.findIndex((file) => file.name === newFilename);
+
+            if (index < 0) {
+                // If the file is a new one, It's simply added to the array.
+                filesList.push(event.target.files[0])   
+            } else {
+                //If not, it's replaced.
+                filesList.splice(index, 1, event.target.files[0]);
+            }
+
+            setUploadedFiles(filesList);
+
+        }
+        console.log(filesList);
+        setUploadActive(false);
+        event.target.value = "";
+    }
+
+    function handleRemoveFile(name) {
+        setUploadedFiles((prevItems) => prevItems.filter((item) => item.name !== name));
+      };
+
     function handleSubmit(event) {
         event.preventDefault();
 
         console.log("Form submitted: ",formState)
+
+        {/* Make use of firebase to handle files uploaded */}
     }
 
     return (
@@ -96,6 +136,43 @@ const NewTicket = () => {
               value={formState.description}
               onChange={onChangeHandler}
             />
+
+            <Box>
+                <Button 
+                  variant="contained" 
+                  color="secondary" 
+                  startIcon={<FileUploadIcon />} 
+                  onClick={() => setUploadActive(true)}
+                  sx={{ display: uploadActive ? 'none' : 'inline-block' }}>
+                    Add file
+                </Button>
+                <Button 
+                  variant="outlined" 
+                  color="secondary"  
+                  onClick={() => setUploadActive(false)}
+                  sx={{ display: uploadActive ? 'inline-block' : 'none' }}>
+                    UNDO
+                </Button>
+            </Box>
+            <Box sx={{ width: '100%'}}>
+                <Input sx={{ display: uploadActive ? 'block' : 'none' }}
+                    component="input"
+                    type="file"
+                    id="file-upload"
+                    accept=".png, .jpg, .jpeg"
+                    onChange={handleFileChange}
+                />
+                {uploadedFiles[0] && <Typography variant="body1" color="initial" sx={{ fontWeight: 'bold', mt: '15px', mb: '5px'}}>Files Uploaded</Typography> }
+                {uploadedFiles[0] && uploadedFiles.map((file, index) => {
+                    return (
+                        <div key={index}>{file.name} <IconButton aria-label="delete" color="error" size="large" onClick={() => handleRemoveFile(file.name)}>
+                          <DeleteIcon />
+                         </IconButton>
+                        </div>
+                    )
+                })}
+            </Box>
+
             <Box sx={{ display: 'flex', justifyContent: 'center', gap: '50px'}}>
                 <Button
                 type="submit"
