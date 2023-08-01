bed7eecd73e77fb389177828cb9c7a7adec9bae4
Tizios9999 <davide.santonocito@outlook.com>
2023-08-01 10:38:30 +0200

    get ticketId from backend after creation

--- src/app/services/ticket.service.js
+++ src/app/services/ticket.service.js
@@ -4,9 +4,21 @@ const API_URL = "http://localhost:8080/api/ticket/";
 
 const createTicket = (ticket) => {
   console.log("ticket sent: ", ticket);
-  return axios.post(API_URL + "post", ticket, {
-    headers: { "Content-Type": "application/json" },
-  });
+  return axios
+    .post(API_URL + "post", ticket, {
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      console.log("Response from server: ", response.data);
+
+      const ticketId = response.data.ticketId;
+
+      return ticketId;
+    })
+    .catch((error) => {
+      console.error("Error creating ticket: ", error);
+      throw error;
+    });
 };
 
 export default createTicket;
