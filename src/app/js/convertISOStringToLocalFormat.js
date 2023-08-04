346492f51e4ff4bebee395251fbc6121375cd83c
Tizios9999 <davide.santonocito@outlook.com>
2023-08-04 11:01:18 +0200

    created an ISO date conversion function

---
+++ src/app/js/convertISOStringToLocalFormat.js
@@ -0,0 +1,15 @@
+function convertISOStringToLocalFormat(isoString) {
+  const date = new Date(isoString);
+  const timezoneOffset = date.getTimezoneOffset();
+  date.setMinutes(date.getMinutes() - timezoneOffset);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${day}-${month}-${year}, ${hours}:${minutes}`;
+}
+
+export default convertISOStringToLocalFormat;
