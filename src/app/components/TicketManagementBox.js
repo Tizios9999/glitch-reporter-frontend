9f2ea01c7e931068efd961d2951f1c8ead422594
Tizios9999 <davide.santonocito@outlook.com>
2023-08-11 14:36:38 +0200

    automated message function now works

--- src/app/components/TicketManagementBox.js
+++ src/app/components/TicketManagementBox.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import { useRouter } from "next/navigation";
+
 import { Box, Typography, Button } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -7,11 +9,14 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
-import { updateTicketStatus } from "../services/ticket.service";
+import { updateTicketStatus, addMessage } from "../services/ticket.service";
+import getCurrentDateTimeISO from "../js/getCurrentDateTimeISO";
 
 import capitalizeString from "../js/capitalizeString";
 
 function TicketManagementBox({ user, ticket, statuses }) {
+  const router = useRouter();
+
   const allowedUserRoles = ["ROLE_ADMIN", "ROLE_AGENT"];
 
   const [managedByThisAgent, setManagedByThisAgent] = useState(
@@ -24,31 +29,94 @@ function TicketManagementBox({ user, ticket, statuses }) {
 
   const [ticketStatusValue, setTicketStatusValue] = useState(ticket.statusId);
 
+  let ticketManagementUserText = "";
+  let statusMessageText = "";
+
   function handleAssignmentButtonClick() {
     if (!managedByThisAgent) {
       setManagedByThisAgent(true);
       setNewAssignedUserId(user.id);
+      ticketManagementUserText = generateAgentText(user.username);
     } else {
       setManagedByThisAgent(false);
       setNewAssignedUserId(ticket.assignedToId);
+      ticketManagementUserText = "";
     }
   }
 
   function handleStatusChange(event) {
     setTicketStatusValue(event.target.value);
+
+    statusMessageText = generateStatusText(
+      user.username,
+      ticketStatusValue,
+      statuses
+    );
   }
 
-  function handleUpdateButtonClick() {
+  async function handleUpdateButtonClick() {
     console.log("Update ticket: ", ticketStatusValue, newAssignedUserId);
 
-    /* If the ticket is still unassigned during the update, 
-       it will be assigned automatically to whoever updates it first. */
-    const updateData = {
-      ticketStatusId: Number(ticketStatusValue),
-      newAssignedUserId: newAssignedUserId == 0 ? user.id : newAssignedUserId,
-    };
+    let statusChanged =
+      ticket.statusId === Number(ticketStatusValue) ? false : true;
+    let assignedUserChanged =
+      Number(newAssignedUserId) === Number(ticket.assignedToId) ? false : true;
 
-    updateTicketStatus(updateData, ticket.ticketId);
+    console.log(statusChanged, assignedUserChanged);
+    console.log("Assigned user id", newAssignedUserId);
+    console.log("ticket id", ticket.assignedToId);
+
+    if (statusChanged || assignedUserChanged || ticket.assignedToId === 0) {
+      /* If the ticket is still unassigned during the update, 
+       it will be assigned automatically to whoever updates it first. */
+      const updateData = {
+        ticketStatusId: Number(ticketStatusValue),
+        newAssignedUserId:
+          ticket.assignedToId === 0 ? user.id : newAssignedUserId,
+      };
+
+      try {
+        await updateTicketStatus(updateData, ticket.ticketId);
+
+        const currentDateTimeISO = getCurrentDateTimeISO();
+
+        statusMessageText = statusChanged
+          ? generateStatusText(user.username, ticketStatusValue, statuses)
+          : "";
+
+        ticketManagementUserText = assignedUserChanged
+          ? generateAgentText(user.username)
+          : "";
+
+        const automatedMessageText = `Automated message: ${ticketManagementUserText} ${statusMessageText}`;
+
+        const automatedMessage = {
+          ticketId: ticket.ticketId,
+          senderId: user.id,
+          sender: user.username,
+          message: automatedMessageText,
+          messageDate: currentDateTimeISO,
+          uploadedFiles: [],
+        };
+
+        await addMessage(automatedMessage, ticket.ticketId);
+
+        // Refresh the page
+      } catch (error) {
+        console.error("Error during update and message addition: ", error);
+        // Handle the error
+      }
+    }
+  }
+
+  function generateAgentText(username) {
+    return `Agent ${username} now is assigned to this ticket.`;
+  }
+
+  function generateStatusText(username, statusValue, statuses) {
+    const currentStatus = statuses.filter((status) => status.id == statusValue);
+
+    return `Agent ${username} has updated the ticket status to <${currentStatus[0].name}>.`;
   }
 
   return (
